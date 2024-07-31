0a688e392a7daacddc287116fe6933392c0e6eb7
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-31 21:07:26 +0300

    completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
+import React from 'react';
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
 
 function App() {
-  return (
-    <div>
-      <NavBar />
-      <Home />
-      <About />
-    </div>
-  );
+	return (
+		<div>
+			<NavBar />
+			<Home />
+			<About />
+		</div>
+	);
 }
 
 export default App;
